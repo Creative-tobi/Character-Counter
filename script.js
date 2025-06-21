1b13215908c6ddc19430594630c8d245d8f02def
Creative-tobi <adeoyeaisha533@gmail.com>
2025-06-21 23:01:24 +0100

    character counter

--- script.js
+++ script.js
@@ -19,3 +19,19 @@ var text = document.getElementById("leave");
 text.addEventListener("input", CharacterCount);
 
 
+
+// verifying gmail
+let check = document.getElementById("text");
+function submit(){
+    if ((check.value).includes("@") && (check.value).includes(".")){
+        document.getElementById("end2").innerHTML = " " ;
+    } else {
+        document.getElementById("end2").innerHTML = " E-mails Only";
+        document.getElementById("end2").style.color = "red";
+    }
+}
+
+//using event listener
+var sub = document.getElementById("text");
+sub.addEventListener("input", submit) //won't need to click button before it work
+
